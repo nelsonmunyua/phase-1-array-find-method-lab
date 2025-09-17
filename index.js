3b7e637fb4c909d64f60b1b5fcff4cf58e4731e1
Nelson Munyua <nelsonmunyua8@gmail.com>
2025-09-17 09:06:58 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+  const win = record.find((game) => game.result === "W");
+  return win ? win.year : undefined;
+}
+
+const record = [
+  { year: "2015", result: "L" },
+  { year: "2016", result: "N/A" },
+  { year: "2017", result: "W" },
+];
